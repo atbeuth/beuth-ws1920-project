7ec4a5d7e920702492986a7a4969ef6f2ecb5468
Cem Saygili <johnnyphoenix2109@gmail.com>
2020-01-03 23:23:40 +0100

    remove exif data from uploaded pictures and cleanup unused data
    1. remove exif data and resize image (if > 4K)
    2. rotate a picture that is taken from a phone and has exif data for the orientation
    3. Automatically delete files for FileField, ImageField, and subclasses. It will delete
    old files when a new file is being saved and it will delete files on model instance deletion.
    
     Please enter the commit message for your changes. Lines starting

--- photo_webs/imageposts/forms.py
+++ photo_webs/imageposts/forms.py
@@ -6,3 +6,4 @@ class ImagepostForm(forms.ModelForm):
     class Meta:
         model = Imagepost
         fields = ['title', 'img', 'description','long_description','freeuse','pinned','category','tags']
+
