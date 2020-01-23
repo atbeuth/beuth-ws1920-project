887eb7ac058a7c02d4c409d4bf5cffa1efec7d14
devmarcstorm <43650740+devmarcstorm@users.noreply.github.com>
2020-01-23 15:20:37 +0100

    Add user settings functionality
    These changes were made to give the user the choice to change their
    informations on our side.
    
    - made edit info button work (profile)
    - made setting button work (header nav)
    
    - settings pages
       - forms for user settings/profile settings/password settings
       - views and templates for all forms
       - updated urls

--- photo_webs/user/urls.py
+++ photo_webs/user/urls.py
@@ -6,8 +6,9 @@ app_name = 'user'
 
 urlpatterns = [
     path('', views.profile,name='profile'),
-    path('<int:pk>/settings/', views.UserSettingsView.as_view(), name='profile-settings'),
-    path('<int:pk>/', views.UserDetailView.as_view(), name='profile'),
+    path('settings/', views.user_edit, name='user-settings'),
+    path('profile/settings/', views.profile_edit, name='profile-settings'),
+    path('settings/password/', views.change_password, name='change_password'),    path('<int:pk>/', views.UserDetailView.as_view(), name='profile'),
     path('<int:pk>/images/', views.UserImagesView.as_view(), name='profile-images'),
     path('<int:pk>/followers/', views.FollowerView.as_view(), name='profile-follows'),
     path('signup/', views.SignUp.as_view(), name='signup'),
